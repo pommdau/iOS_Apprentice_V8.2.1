1e209d76350db935f29cfcbefd124c2be6cb0606
IKEH <pondau1024@gmail.com>
2020-03-15 13:44:06 +0900

    P1042

---
+++ Practice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Hiroki Ikeuchi on 2020/03/15.
+//  Copyright © 2020 ikeh1024. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
