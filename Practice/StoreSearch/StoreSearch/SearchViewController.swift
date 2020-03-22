3794ea6a84fd6c4f116843aa839866a9f0b4f23c
IKEH <pondau1024@gmail.com>
2020-03-22 19:49:39 +0900

    P1125

--- Practice/StoreSearch/StoreSearch/SearchViewController.swift
+++ Practice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -96,6 +96,11 @@ extension SearchViewController: UISearchBarDelegate {
             
             if let data = performStoreRequest(with: url) {
                 searchResults = parse(data: data)
+                // A~Zの順にソートする
+                searchResults.sort(by: { result1, result2 in
+                    return result1.name.localizedStandardCompare(result2.name) == .orderedAscending
+                })
+
             }
             tableView.reloadData()
         }
