581077047ace5ca3712f8048972035889d669d7b
IKEH <pondau1024@gmail.com>
2020-03-22 19:41:42 +0900

    P1124

--- Practice/StoreSearch/StoreSearch/SearchResult.swift
+++ Practice/StoreSearch/StoreSearch/SearchResult.swift
@@ -29,8 +29,22 @@ class ResultArray: Codable {
 class SearchResult: Codable, CustomStringConvertible {
 
     var kind : String? = ""  // Audiobooksの場合はnil
-    var type : String {
-        return kind ?? "audiobook"
+    var type:String {   // 表示する種類をユーザフレンドリにする
+        let kind = self.kind ?? "audiobook"
+        switch kind {
+        case "album"        : return "Album"
+        case "audiobook"    : return "Audio Book"
+        case "book"         : return "Book"
+        case "ebook"        : return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video"  : return "Music Video"
+        case "podcast"      : return "Podcast"
+        case "software"     : return "App"
+        case "song"         : return "Song"
+        case "tv-episode"   : return "TV Episode"
+        default             : break
+        }
+        return "Unknown"
     }
     
     var artistName: String? = ""
