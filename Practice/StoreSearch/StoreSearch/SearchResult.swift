dee83022f754fc3a2d1674e262c94bc9bf542642
IKEH <pondau1024@gmail.com>
2020-03-22 15:18:54 +0900

    P1111

--- Practice/StoreSearch/StoreSearch/SearchResult.swift
+++ Practice/StoreSearch/StoreSearch/SearchResult.swift
@@ -8,7 +8,16 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+}
+
+class SearchResult: Codable {
+    var artistName: String? = ""  // 返ってくるJSONデータは必ずしもデータを含まないのでオプショナル型にする
+    var trackName: String? = ""
+    
+    var name: String {
+        return trackName ?? ""
+    }
 }
