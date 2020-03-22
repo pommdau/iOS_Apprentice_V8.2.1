2e05999765cd73d86cee8f3a1b155cbb91bdcce1
IKEH <pondau1024@gmail.com>
2020-03-22 17:11:01 +0900

    P1112

--- Practice/StoreSearch/StoreSearch/SearchResult.swift
+++ Practice/StoreSearch/StoreSearch/SearchResult.swift
@@ -15,34 +15,56 @@ class ResultArray: Codable {
 
 // CustomStringConvertible: the protocol allows objects to have a custom string describing the object, or its contents.
 class SearchResult: Codable, CustomStringConvertible {
-    var kind: String? = ""
+    var kind      : String? = ""
     var artistName: String? = ""  // 返ってくるJSONデータは必ずしもデータを含まないのでオプショナル型にする
+    var currency   = ""
     
-    var name: String {
-        return trackName ?? ""
+    var trackName     : String? = ""
+    var collectionName: String?
+    var name          : String {
+        return trackName ?? collectionName ?? ""
     }
-    
-    var description: String {
-        return "Kind: \(kind ?? "None")  Name: \(name), Artist Name: \(artistName ?? "None")"
-    }
-    
-    var trackName: String? = ""
-    
-    var trackPrice: Double? = 0.0
-    var currency = ""
-    
+        
     var imageSmall = ""
     var imageLarge = ""
-    var storeURL: String? = ""
-    var genre = ""
+
+    var trackViewUrl     : String?
+    var collectionViewUrl: String?
+    var storeURL         : String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+    
+    var trackPrice     : Double? = 0.0
+    var collectionPrice: Double?
+    var itemPrice      : Double?
+    var price          : Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+    
+    var itemGenre: String?
+    var bookGenre: [String]?
+    var genre    : String {
+        if let genre = itemGenre {  // e-book以外
+            return genre
+        } else if let genres = bookGenre {  // e-book用
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
     
     // JSONのキー名とプロパティ名が異なるのでCodingKeysを使用する
     enum CodingKeys: String, CodingKey {
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
-        case storeURL = "trackViewUrl"
-        case genre = "primaryGenreName"
-        case kind, artistName, trackName
-        case trackPrice, currency
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
+    }
+    
+    var description: String {
+        return "Kind: \(kind ?? "None")  Name: \(name), Artist Name: \(artistName ?? "None")"
     }
 }
