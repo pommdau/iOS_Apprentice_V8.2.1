2661cef7cfc6745dddbe5651ee8afa801f541c9d
hikeuchi <hikeuchi@morisawa.co.jp>
2020-03-27 14:54:15 +0900

    P1201

--- Practice/StoreSearch/StoreSearch/DetailViewController.swift
+++ Practice/StoreSearch/StoreSearch/DetailViewController.swift
@@ -64,6 +64,13 @@ class DetailViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func openInStore() {
+        if let url = URL(string: searchResult.storeURL) {
+            UIApplication.shared.open(url, options: [:],
+                                      completionHandler: nil)
+        }
+    }
+    
     
     // MARK:- Helper Methods
     func updateUI() {
