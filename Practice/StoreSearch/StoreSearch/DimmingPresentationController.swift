76e82af2edafab4c16087a89c55c9a784cb80482
IKEH <pondau1024@gmail.com>
2020-03-29 21:57:22 +0900

    P1225

--- Practice/StoreSearch/StoreSearch/DimmingPresentationController.swift
+++ Practice/StoreSearch/StoreSearch/DimmingPresentationController.swift
@@ -16,6 +16,24 @@ class DimmingPresentationController: UIPresentationController {
     override func presentationTransitionWillBegin() {
         dimmingView.frame = containerView!.bounds
         containerView!.insertSubview(dimmingView, at: 0)
+        
+        // Animate background gradient view(fade in)
+        dimmingView.alpha = 0
+        if let corrdinator = presentedViewController.transitionCoordinator {
+            // all of your animations should be done in a closure passed to animateAlongsideTransition to keep the transition smooth.
+            corrdinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 1
+            }, completion: nil)
+        }
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        if let coordinator = presentedViewController.transitionCoordinator {
+            // fade outのアニメーション
+            coordinator.animate(alongsideTransition: {_ in
+                self.dimmingView.alpha = 0
+            }, completion: nil)
+        }
     }
     
     override var shouldRemovePresentersView: Bool {
