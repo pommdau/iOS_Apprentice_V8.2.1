85b219d0bfe5f3d52d39e813c1a4ff8815852f21
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-01 16:51:40 +0900

    P1251

--- Practice/StoreSearch/StoreSearch/LandscapeViewController.swift
+++ Practice/StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+    @IBOutlet weak var scrollView : UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Remove constraints from main view
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        // Remove constraints for page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        // Remove constraints for scroll view
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let safeFrame = view.safeAreaLayoutGuide.layoutFrame
+        scrollView.frame = safeFrame  // safe areaいっぱいに広げる
+        pageControl.frame = CGRect(x: safeFrame.origin.x,
+                                   y: safeFrame.size.height - pageControl.frame.size.height,
+                                   width: safeFrame.size.width,
+                                   height: pageControl.frame.size.height)
     }
     
-
     /*
     // MARK: - Navigation
 
