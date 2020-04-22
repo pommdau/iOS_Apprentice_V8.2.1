9153b55afd8333d9e09c045dc858c69cb56bdcc1
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-22 11:43:26 +0900

    create project

---
+++ Practice/MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by HIROKI IKEUCHI on 2020/04/22.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Practice/MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by HIROKI IKEUCHI on 2020/04/22.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
