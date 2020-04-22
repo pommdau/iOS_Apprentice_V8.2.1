12ea12e9ed73b82c77adca5a4016e67ef8f61cd2
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-22 13:12:50 +0900

    P756

--- Practice/MyLocations/MyLocations/CurrentLocationViewController.swift
+++ Practice/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -29,8 +29,14 @@ class CurrentLocationViewController: UIViewController {
     // MARK:- Actions
     @IBAction func getLocation() {
         let authStatus = CLLocationManager.authorizationStatus()
-        if authStatus == .notDetermined {  // まだ許可をリクエストしていない場合
+        // .notDetermined: まだ許可をリクエストしていない場合
+        if authStatus == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
             return
         }
         
@@ -39,12 +45,27 @@ class CurrentLocationViewController: UIViewController {
         locationManager.startUpdatingLocation()
     }
     
+    
+    // MARK:- Helper Methods
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled",
+                                      message: "Please enable location services for this app in Settings",
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 
 // MARK:- CLLocationManagerDelegate
 extension CurrentLocationViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // e.g. didFailWithError The operation couldn’t be completed. (kCLErrorDomain error 1.)
+        // This Error object has a domain and a code.
+        // The domain in this case is kCLErrorDomain meaning the error came from Core Location (CL). The code is 1,
+        // also identified by the symbolic name CLError.denied, which means the user did not allow the app to obtain location information.
         print("didFailWithError \(error.localizedDescription)")
     }
     
