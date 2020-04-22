26df1cc7db4b2fd767d60ff848d5695b14423a52
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-22 17:32:57 +0900

    P814

--- Practice/MyLocations/MyLocations/CurrentLocationViewController.swift
+++ Practice/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -37,6 +37,16 @@ class CurrentLocationViewController: UIViewController {
         updateLabels()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     
     // MARK:- Actions
     @IBAction func getLocation() {
---
+++ Practice/MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by HIROKI IKEUCHI on 2020/04/22.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    // MARK:- Actions
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
+
+
