3a4530c107ad93c60a644f74f06d7b1c36f4597a
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-22 13:03:31 +0900

    P753

---
+++ Practice/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,57 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by HIROKI IKEUCHI on 2020/04/22.
+//  Copyright © 2020 hikeuchi. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK:- Actions
+    @IBAction func getLocation() {
+        let authStatus = CLLocationManager.authorizationStatus()
+        if authStatus == .notDetermined {  // まだ許可をリクエストしていない場合
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+    }
+    
+}
+
+
+// MARK:- CLLocationManagerDelegate
+extension CurrentLocationViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+    }
+    
+}
+
