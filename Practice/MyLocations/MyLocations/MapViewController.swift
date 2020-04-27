8dd30d36df420bb0611f2cff279a206efd97f3c1
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-27 09:45:25 +0900

    P949

--- Practice/MyLocations/MyLocations/MapViewController.swift
+++ Practice/MyLocations/MyLocations/MapViewController.swift
@@ -27,6 +27,18 @@ class MapViewController: UIViewController {
     }
     
     
+    // MARK:- Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.managedObjectContext = managedObjectContext
+            
+            let button = sender as! UIButton
+            let location = locations[button.tag]
+            controller.locationToEdit = location
+        }
+    }
+    
     // MARK:- Actions
     @IBAction func showUser() {
         let region = MKCoordinateRegion(
@@ -44,7 +56,7 @@ class MapViewController: UIViewController {
     
     // pinの(i)ボタン押下時のアクション
     @objc func showLocationDetails(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: "EditLocation", sender: sender)
     }
     
     // MARK:- Heloper methods
