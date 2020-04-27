7e333e23083c8f0899a6625537109699764f0316
hikeuchi <hikeuchi@morisawa.co.jp>
2020-04-27 15:32:35 +0900

    P983

--- Practice/MyLocations/MyLocations/Location+CoreDataClass.swift
+++ Practice/MyLocations/MyLocations/Location+CoreDataClass.swift
@@ -42,6 +42,17 @@ public class Location: NSManagedObject, MKAnnotation {
         return currentID
     }
     
+    func removePhotoFile() {
+        if hasPhoto {
+            do {
+                try FileManager.default.removeItem(at: photoURL)
+            } catch {
+                print("Error removing file: \(error)")
+            }
+        }
+    }
+    
+    
     // MARK:- MKAnnotation Property
     // MKAnnotationプロトコルに必須のプロパティ
     public var coordinate: CLLocationCoordinate2D {
